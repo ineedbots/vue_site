35d6b1815db2b9a9c85b9237a9f3527f8c4fb579
INeedBots <ineedbots@outlook.com>
2021-09-02 21:49:19 -0600

    vscode eslint airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,18 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false
+    requireConfigFile: false,
   },
   extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:vue/essential',
+    '@vue/airbnb',
   ],
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
-}
+  rules: {},
+};
