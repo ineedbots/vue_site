34c76d174a0d87367ee6d800b81b66acd782514a
INeedBots <ineedbots@outlook.com>
2021-09-07 19:45:46 -0600

    cod2m

--- pages/cod2m.vue
+++ pages/cod2m.vue
@@ -5,7 +5,23 @@
         <p class="title">
           CoD2M
         </p>
-        <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+        <p>
+          <img src="/cod2.png" alt="CoD2" style=""/>
+          <br>
+          <br>
+          CoD2M is a modification for the Windows videogame
+          <a href="https://store.steampowered.com/app/2630/Call_of_Duty_2/">Call of Duty 2</a>.
+          <br><br>
+          It builds into a D3D9 DLL proxy (thanks momo5502) that runs when the game starts up.
+          It uses C++ and x86 assembly to perform the modications to the game.
+          <br><br>
+          The main purpose of this project is to allow my
+          <NuxtLink to="/bot-warfare">Bot Warfare</NuxtLink>
+          mod to work with CoD2, but it also adds a lot of other features to the game.
+          <br><br>
+          <br>
+          <a href="https://github.com/ineedbots/cod2m/">GitHub</a>
+        </p>
       </div>
 
       <div style="height: 40px" />
