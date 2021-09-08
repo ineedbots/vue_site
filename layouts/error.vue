2837cc164a9020f87bbc0b060e66f9c6aac7950d
INeedBots <ineedbots@outlook.com>
2021-09-07 18:49:35 -0600

    Added error layout

---
+++ layouts/error.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="ing-content nes-container">
+      <h1 v-if="error.statusCode === 404">
+        {{ pageNotFound }}
+      </h1>
+      <h1 v-else>
+        {{ otherError }}
+      </h1>
+      <NuxtLink to="/">
+        Go back perhaps?
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      pageNotFound: 'Lost?',
+      otherError: 'How did this happen??',
+    };
+  },
+  head() {
+    const title = this.error.statusCode === 404 ? this.pageNotFound : this.otherError;
+    return {
+      title,
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 30px;
+}
+* {
+  text-align: center;
+}
+</style>
